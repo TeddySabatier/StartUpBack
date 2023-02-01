860a352c539ea83b915063980b47d47f8984fd9c
MASSET <yoann.masset.pro@gmail.com>
2023-02-01 17:42:42 +0100

    Modify Test

--- src/test/java/fr/tse/startuppoc/project/controller/TestUserController.java
+++ src/test/java/fr/tse/startuppoc/project/controller/TestUserController.java
@@ -9,7 +9,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
--- src/test/java/fr/tse/startuppoc/project/service/TestTimeDayService.java
+++ src/test/java/fr/tse/startuppoc/project/service/TestTimeDayService.java
@@ -4,8 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.runner.RunWith;
@@ -22,7 +22,7 @@ import fr.tse.startuppoc.project.entity.TimeDay;
 @RunWith(SpringRunner.class)
 @ActiveProfiles(profiles="test")
 @TestMethodOrder(OrderAnnotation.class) // Define the order in which tests are executed
-class TestTimeDayService {
+public class TestTimeDayService {
 	
 	@Autowired
 	private TimeDayService _timeDayService;
@@ -31,13 +31,13 @@ class TestTimeDayService {
 
 	@Test
 	@Order(1) // First test to be executed
-	void findAllTimeDayTest() {
+	public void findAllTimeDayTest() {
 		assertEquals(4, this._timeDayService.findAllTimeDay().size());
 	}
 
 	@Test
 	@Order(2)
-	void findByIdTest() {
+	public void findByIdTest() {
 		TimeDay timeDay1 = this._timeDayService.findAllTimeDay().get(0);
 		TimeDay timeDay2 = this._timeDayService.findById(1L);
 		assertEquals(timeDay1.getDate(), timeDay2.getDate());
@@ -46,28 +46,28 @@ class TestTimeDayService {
 	
 	@Test
 	@Order(3)
-	void findByUserIdTest() {
+	public void findByUserIdTest() {
 		List<TimeDay> timeDay4UserId1 = this._timeDayService.findByUserId(1L);
 		assertEquals(2, timeDay4UserId1.size());
 	}
 	
 	@Test
 	@Order(4)
-	void findByProjectIdTest() {
+	public void findByProjectIdTest() {
 		List<TimeDay> timeDay4ProjectId1 = this._timeDayService.findByProjectId(1L);
 		assertEquals(2, timeDay4ProjectId1.size());
 	}
 	
 	@Test
 	@Order(5)
-	void findByUserIdAndProjectIdTest() {
+	public void findByUserIdAndProjectIdTest() {
 		List<TimeDay> timeDay4UserId1AndProjectId1 = this._timeDayService.findByUserIdAndProjectId(1L, 1L);
 		assertEquals(1, timeDay4UserId1AndProjectId1.size());
 	}
 	
 	@Test
 	@Order(6)
-	void addTimeDayTest() {
+	public void addTimeDayTest() {
 		int initialSize = this._timeDayService.findAllTimeDay().size();
 		TimeDay newTimeDay = new TimeDay();
 		TimeDay addedTimeDay = this._timeDayService.addTimeDay(newTimeDay);
@@ -79,7 +79,7 @@ class TestTimeDayService {
 	
 	@Test
 	@Order(7)
-	void deleteOneTimeDayTest() {
+	public void deleteOneTimeDayTest() {
 		int initialSize = this._timeDayService.findAllTimeDay().size();
 		TimeDay timeDay = this._timeDayService.findById(addedTimeDayId);
 		assertNotNull(timeDay);
